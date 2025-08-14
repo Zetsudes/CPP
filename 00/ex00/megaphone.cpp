74a5ef58a7cd3269bd8bf06bb4f6edfb8e839f61
Zainab Mohamed <zamohame@c2r12p2.42nice.fr>
2025-08-14 12:50:11 +0200

    first commit

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+
+	for (int i = 1; i < argc; ++i)
+	{
+		std::string arg = argv[i]; // converts argv[i] to a c++ string
+
+		for (size_t j = 0; j < arg.length(); ++j)
+			arg[j] = std::toupper(arg[j]);
+
+		std::cout << arg;
+	}
+	std::cout << "\n";
+}
