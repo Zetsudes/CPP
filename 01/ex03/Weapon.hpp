5ae646f57910af9fb70bf1ea911a17357bd92951
Zainab Mohamed <zamohame@c1r8p1.42nice.fr>
2025-09-09 19:49:06 +0200

    things

--- 01/ex03/Weapon.hpp
+++ 01/ex03/Weapon.hpp
@@ -1,10 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zamohame <zamohame@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 09:47:45 by zamohame          #+#    #+#             */
+/*   Updated: 2025/09/09 15:18:57 by zamohame         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
 
 #include <iostream>
-#include <cctype>
-#include <string>
 
 class Weapon
 {
@@ -12,8 +21,9 @@ class Weapon
         std::string type;
     
     public:
+		Weapon(std::string &type);
         void setType(const std::string &str);
-        std::string& getType() const;
+        const std::string& getType() const;
 
 };
 
