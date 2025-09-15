ae83e2848c2ee30eb20b0818cabb00e90d46452a
Zainab Mohamed <zamohame@c2r11p1.42nice.fr>
2025-09-15 10:54:27 +0200

    tests

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -14,7 +14,7 @@
 
 int main (void)
 {
-    int N = 3;
+    int N = 10;
     Zombie* horde = zombieHorde(N, "Oggy");
 
     for (int i = 0; i < N; i++)
--- 01/ex04/replace.hpp
+++ 01/ex04/replace.hpp
@@ -7,6 +7,6 @@
 
 bool    parseArgs(int argc, char **argv, std::string &filename, std::string &s1, std::string &s2);
 bool    parseFile(const std::string &filename);
-std::string replaceLine(const std::string &line, const std::string &s1, const std::string s2);
+std::string replaceLine(const std::string &line, const std::string &s1, const std::string &s2);
 
 #endif
